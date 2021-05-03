787438e55e85afcd2cf37f058cbe716ab2250cb8
Christopher Duggan <christopherduggan@gmail.com>
2021-05-03 15:34:18 -0400

    implement attackOpponent() method

--- pokemon.js
+++ pokemon.js
@@ -14,6 +14,13 @@ class Pokemon {
     if (this.health < 0) this.health = 0;
     return this.health;
   }
+
+  attackOpponent(opponent) {
+    let damage = this.attack - opponent.defense;
+    if (damage < 1) damage = 1;
+    opponent.takeDamage(damage);
+  }
+
 }
 
 // Don't edit this line!
