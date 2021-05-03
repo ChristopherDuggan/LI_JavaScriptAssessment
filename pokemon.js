c3dc13f6882338702a2b16f029a27aeef19b0dad
Christopher Duggan <christopherduggan@gmail.com>
2021-05-03 13:07:15 -0400

    define Pokemon class

--- pokemon.js
+++ pokemon.js
@@ -1,7 +1,14 @@
 // Your code here!
 
-
-
+class Pokemon {
+  constructor(name, attack, defense, health, type) {
+    this.name = name;
+    this.attack = attack;
+    this.defense = defense;
+    this.health = health;
+    this.type = type;
+  }
+}
 
 // Don't edit this line!
 module.exports = Pokemon;
